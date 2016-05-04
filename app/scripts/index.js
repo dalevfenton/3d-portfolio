935a117bd6aefb0721b2f6a1366e54e8035be081
Dale Fenton <fentondale@gmail.com>
2016-05-04 17:17:09 -0400

    hooking into wordpress api

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,8 +1,15 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var $ = require('jquery');
 
 var Controller = require('./components/controller.jsx');
 
+var apiURL = 'http://dalefenton.com/wp-json/wp/v2/posts';
+$.ajax(apiURL).then(function(data){
+  console.log(data);
+}, function(error){
+  console.log(error);
+});
 ReactDOM.render(
   React.createElement( Controller, {} ),
   document.getElementById('app')
